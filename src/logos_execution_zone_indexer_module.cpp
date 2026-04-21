825ee1d6f7cbb7c3a416c4d0798edee88a9d0ee0
Pravdyvy <awxrvtb@gmail.com>
2026-04-21 17:12:53 +0300

    fix: build success

--- src/logos_execution_zone_indexer_module.cpp
+++ src/logos_execution_zone_indexer_module.cpp
@@ -11,9 +11,9 @@ LogosExecutionZoneIndexerModule::LogosExecutionZoneIndexerModule() = default;
 
 LogosExecutionZoneIndexerModule::~LogosExecutionZoneIndexerModule() {
     if (indexer_service_ffi) {
-        operation_result = indexer_ffi::stop_indexer(indexer_service_ffi);
+        OperationStatus operation_result = stop_indexer(indexer_service_ffi);
 
-        if (indexer_ffi::is_error(&operation_result)) {
+        if (is_error(&operation_result)) {
             int signal = operation_result;
             qWarning() << "destructor: indexer FFI error: " << signal;
         }
@@ -41,21 +41,21 @@ void LogosExecutionZoneIndexerModule::initLogos(LogosAPI* logosApiInstance) {
 // === Indexer Lifecycle ===
 
 int LogosExecutionZoneIndexerModule::start_indexer(const QString& config_path, uint16_t port) {
-    if !(indexer_service_ffi) {
+    if (!indexer_service_ffi) {
         QByteArray utf8 = config_path.toUtf8();
         const char* c_path = utf8.constData();
 
-        indexer_service_ffi_res = indexer_ffi::start_indexer(c_path, port);
+        InitializedIndexerServiceFFIResult indexer_service_ffi_res = ::start_indexer(c_path, port);
 
-        if (indexer_ffi::is_error(&indexer_service_ffi_res.error)) {
+        if (is_error(&indexer_service_ffi_res.error)) {
             int signal = indexer_service_ffi_res.error;
             qWarning() << "start_indexer: indexer FFI error: " << signal;
 
             return signal;
         }
 
-        indexer_service_ffi = indexer_service_ffi_res.value
-
-        return 0;
+        indexer_service_ffi = indexer_service_ffi_res.value;
     }
+
+    return 0;
 }
