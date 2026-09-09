282668b94b4298e1614234b32c0b1a073dfd7a3f
erhant <erhany96@gmail.com>
2026-09-09 20:39:22 +0300

    chore: bump flakes

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
 
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder";
-    logos-execution-zone.url = "github:logos-blockchain/logos-execution-zone?ref=v0.2.5-rc1";
+    logos-execution-zone.url = "github:logos-blockchain/logos-execution-zone?ref=v0.2.5-rc2";
   };
 
   outputs =
